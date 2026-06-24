497e487e136919f7a4373c2fa1c8bef3fc9f3530
ANONYMOUS <drivbotwbs@gmail.com>
2026-06-24 17:25:40 +0530

    fix: error

--- src/pages/home/FeaturedProducts.jsx
+++ src/pages/home/FeaturedProducts.jsx
@@ -43,7 +43,7 @@ const FeaturedProducts = () => {
                 setLoading(false);
             }
         };
-        fetchFeatured();
+        fetchFeaturedProducts();
     }, []);
 
     return (
